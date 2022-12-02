448aa1ab3043cc1ea88f16eb8639ffd80f1f87a3
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2022-12-01 20:03:22 -0600

    Fixed cors port

--- public/JS/main.js
+++ public/JS/main.js
@@ -7,7 +7,7 @@ import { scene, camera, renderer, controls } from "./scene.js";
 
 // ----------- [ HTTP REQUESTS DEPS ] -----------
 const STEP_LOCATION = "/step";
-const CORS_URL = "http://0.0.0.0:8080/";
+const CORS_URL = "http://0.0.0.0:8000/";
 const BASE_URL = "https://city-pipeline-lean-squirrel.mybluemix.net"; // TODO: Change to ibmcloud URL
 
 
