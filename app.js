21d534820abf9b946e8af41d6c5ecb642175c462
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2022-12-01 19:54:14 -0600

    Changed port to 8000

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cors_proxy = require('cors-anywhere');
 
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8000;
 
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
