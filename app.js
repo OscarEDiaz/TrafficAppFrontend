858e3fcaa7a946f88c6acf1498f06a3f5094c1a7
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2022-12-01 20:22:05 -0600

    Deleted cors-anywhere proxy

--- app.js
+++ app.js
@@ -2,19 +2,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors_proxy = require('cors-anywhere');
-
-var host = process.env.HOST || '127.0.0.1';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8000;
-
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
 
 var indexRouter = require('./routes/index');
 
